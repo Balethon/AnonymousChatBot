781812d8889fb87e2a7ee4cbc9e6226820a524e9
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2024-01-15 15:35:05 +0330

    Added more handlers to main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from balethon import Client
+from balethon import Client, conditions
 from balethon.objects import Message
 
 import config
@@ -6,9 +6,32 @@ import config
 bot = Client(config.TOKEN)
 
 
-@bot.on_message()
-async def answer_message(message: Message):
-    await message.reply(message.text)
+@bot.on_command()
+async def start(*, message: Message):
+    await message.reply("Hello")
+
+
+@bot.on_message(conditions.at_state(None))
+async def none_state(message: Message):
+    await message.reply("give name")
+    message.author.set_state("NAME")
+
+
+@bot.on_message(conditions.at_state("NAME"))
+async def name_state(message: Message):
+    await message.reply("give age")
+    message.author.set_state("AGE")
+
+
+@bot.on_message(conditions.at_state("AGE"))
+async def age_state(message: Message):
+    await message.reply("salam")
+    message.author.set_state("MAIN")
+
+
+@bot.on_message(conditions.at_state("MAIN"))
+async def main_state(message: Message):
+    await message.reply("you are in main")
 
 
 if __name__ == "__main__":
